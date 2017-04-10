20a492ec4ce76fc5252682608df8b9b41dede1c4
anandmsmaven <anandmsmaven@gmail.com>
2017-04-10 16:36:01 -0400

    sending back google url instead of local url

--- index.js
+++ index.js
@@ -46,16 +46,11 @@ db.serialize(function() {
 					// when the client emits 'receive message', this listens and executes
 					socket_io.on('receive message', function (data) {
 
-						var fs = require('fs');
-						var gm = require('google-static-map').set('AIzaSyBfJkwgvA3XKkS5Y5dHl4gF6e5GjW56HoA');
-						var stream = gm().address(data.latitude + ', ' + data.longitude).staticMap().done(function(){
-							bot.send(Bot.Message.picture('https://ananbh.herokuapp.com/image/map.png')
-								.setAttributionName('Current Location')
-								.setAttributionIcon('http://s.imgur.com/images/favicon-96x96.png'),
-								message.from);
+						bot.send(Bot.Message.picture("https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%50Ccolor:red%7C"+data.latitude+","+data.longitude+"&key=AIzaSyBfJkwgvA3XKkS5Y5dHl4gF6e5GjW56HoA")
+							.setAttributionName('Current Location')
+							.setAttributionIcon('http://s.imgur.com/images/favicon-96x96.png'),
+							message.from);
 
-						});
-						stream.pipe(fs.createWriteStream(__dirname + '/www/image/map.png'));
 
 					});
 				} else {
